6c0b66dd287d6403189bbe60d607f3f2176dd632
shamar <weekesshamar38@gmail.com>
2025-03-29 14:45:53 -0400

    Implemented the quickSort algorithm for integers and any type

--- src/src/QuickSort/AnyTypeQuickSort.java
+++ src/src/QuickSort/AnyTypeQuickSort.java
@@ -1,9 +1,52 @@
 package QuickSort;
 
-public class AnyTypeQuickSort <T extends Comparable>
+public class AnyTypeQuickSort <T extends Comparable<T>>
 {
-    public void quickSort(T[] A, int lowerBound, int upperBound){}
-    public int randomizedPartition(T[] A, int lowerBound, int upperBound){}
-    public int partition(T[] A, int lowerBound,int upperBound){}
+        public void quickSort(T[] A, int lowerBound, int upperBound)
+        {
+            if(lowerBound < upperBound)
+            {
+                int q = partition(A, lowerBound, upperBound);
+                quickSort(A, lowerBound, q - 1);
+                quickSort(A, q + 1, upperBound);
+            }
+        }
+        public int randomizedPartition(T[] A, int lowerBound, int upperBound)
+        {
+            int i= lowerBound+(int)(Math.random()*(upperBound-lowerBound+1));
+            T temp= A[i];
+            A[i]=A[upperBound];
+            A[upperBound]=temp;
+            return partition(A,lowerBound,upperBound);
+        }
+        public void randomizedSort(T []A,int lowerBound, int upperBound)
+        {
+            if(lowerBound< upperBound)
+            {
+                int q = randomizedPartition(A, lowerBound, upperBound);
+                randomizedSort(A, lowerBound, q - 1);
+                randomizedSort(A, q + 1, upperBound);
+            }
+        }
+        public int partition(T[] A, int lowerBound,int upperBound)
+        {
+            T x = A[upperBound];
+            int i =lowerBound-1;
+            for(int j=lowerBound; j< upperBound; j++)
+            {
+                if(A[j].compareTo(x) <=0)
+                {
+                    i++;
+                    T  temp = A[i];
+                    A[i]= A[j];
+                    A[j]=temp;
+                }
+            }
+            T temp=A[i+1];
+            A[i+1]= A[upperBound];
+            A[upperBound]=temp;
+            return i+1;
+        }
 
 }
+
--- src/src/QuickSort/IntegerQuickSort.java
+++ src/src/QuickSort/IntegerQuickSort.java
@@ -2,8 +2,50 @@ package QuickSort;
 
 public class IntegerQuickSort
 {
-    public void quickSort(int[] A, int lowerBound, int upperBound){}
-    public int randomizedPartition(int[] A, int lowerBound, int upperBound){}
-    public int partition(int[] A, int lowerBound,int upperBound){}
+    public void quickSort(int[] A, int lowerBound, int upperBound)
+    {
+        if(lowerBound < upperBound)
+        {
+            int q = partition(A, lowerBound, upperBound);
+            quickSort(A, lowerBound, q - 1);
+            quickSort(A, q + 1, upperBound);
+        }
+    }
+    public int randomizedPartition(int[] A, int lowerBound, int upperBound)
+    {
+        int i= lowerBound+(int)(Math.random()*(upperBound-lowerBound+1));
+        int temp= A[i];
+        A[i]=A[upperBound];
+        A[upperBound]=temp;
+        return partition(A,lowerBound,upperBound);
+    }
+    public void randomizedSort(int []A,int lowerBound, int upperBound)
+    {
+        if(lowerBound< upperBound)
+        {
+            int q = randomizedPartition(A, lowerBound, upperBound);
+            randomizedSort(A, lowerBound, q - 1);
+            randomizedSort(A, q + 1, upperBound);
+        }
+    }
+    public int partition(int[] A, int lowerBound,int upperBound)
+    {
+        int x = A[upperBound];
+        int i =lowerBound-1;
+        for(int j=lowerBound; j< upperBound; j++)
+        {
+            if(A[j] <= x)
+            {
+                i++;
+                int  temp = A[i];
+                A[i]= A[j];
+                A[j]=temp;
+            }
+        }
+        int temp=A[i+1];
+        A[i+1]= A[upperBound];
+        A[upperBound]=temp;
+        return i+1;
+    }
 
 }
